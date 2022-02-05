382039e829f4dba26086662c6ef047a42962dc37
Rushikesh Dehankar <realrushi.dehankar@gmail.com>
2022-02-05 15:11:21 +0530

    MERN stack setup (Hello World)

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React, {Fragment} from "react";
+import './App.css';
+
+const App = () =>
+    <Fragment>
+      <h1>IECDB Hello World</h1>
+    </Fragment>
+
+export default App;
---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const app =express();
+connectDB();
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,() => console.log(`Server started at ${PORT}`));
